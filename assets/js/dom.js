f07b2806cd7c824062e585c663608a58b9f16dd1
Rich <rich@brassbot.com>
2016-04-02 11:22:36 +0100

    Updates after client feedback

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -3,8 +3,18 @@
 var loadNav = function() {
   $('nav[role="navigation"] h2').on('click', 'a', function(e) {
     e.preventDefault();
-    $(this).toggleClass('icon-menu icon-times');
-    $('body').toggleClass('nav-open');
+    $('body').addClass('nav-open');
+    $('nav[role="navigation"] div.nav-wrap').prepend('<a href="#" class="icon-times close-nav">Close Menu</a>');
+
+    closeNav();
+  });
+};
+
+var closeNav = function() {
+  $('nav[role="navigation"]').on('click', 'a.close-nav', function(e) {
+    e.preventDefault();
+    $('body').removeClass('nav-open');
+    $(this).remove();
   });
 };
 
